b4b608fce9cd5e41820b0c2745ab5c8f38230fce
thierry_otvl <tbeigbeder.nospam@gmail.com>
2025-06-03 19:48:26 +0200

    mc

--- install_scripts/common/as_deb_install_ansible.sh
+++ install_scripts/common/as_deb_install_ansible.sh
@@ -6,6 +6,28 @@ sd=`dirname $rp`
 . $sd/../locenv
 ## endpre
 
+install_ansible_config() {
+  cat > ansible.cfg <<EOF
+[defaults]
+deprecation_warnings=False
+vault_identity_list = otvl@~/.config/.otvl/.secrets/otvl_lops_an_vl.txt
+
+[inventory]
+enable_plugins = ini, yaml
+
+EOF
+
+}
+
+gen_ansible_hosts() {
+  hf=".config/.otvl/hosts.yml"
+  for g in `cat .config/.otvl/install_groups` ; do
+    echo "$g:"
+    echo "  hosts:"
+    echo "    localhost:"
+  done
+}
+
 as_deb_install_ansible() {
   cd $sd/../../ansible && \
   cmd make venv-ins && \
@@ -14,6 +36,15 @@ as_deb_install_ansible() {
   cmd cd $HOME/locgit && \
   cmd rm -rf `basename $1` && \
   cmd git clone --single-branch $1 && \
+  cd $sd/../../ansible && \
+  cmd install_ansible_config && \
+  cd && \
+  cmd rm -f .ssh/id_lans && \
+  cmd ssh-keygen -t ed25519 -C otvl-lans -f ~/.ssh/id_lans -q -N '' && \
+  cmd cat .ssh/id_lans.pub >> .ssh/authorized_keys && \
+  cmd ssh -i .ssh/id_lans -o StrictHostKeyChecking=no localhost true && \
+  cmd mkdir -p .config/.otvl/ansible && \
+  cmd gen_ansible_hosts && \
   true
   return $?
 }
--- install_scripts/common/install_ansible.sh
+++ install_scripts/common/install_ansible.sh
@@ -11,6 +11,8 @@ install_ansible() {
   cmd mkdir -p /home/debian/.config/.otvl/.secrets && \
   get_secret ansible_vault_pass > /home/debian/.config/.otvl/.secrets/otvl_lops_an_vl.txt && \
   get_secret ghp_creds > /home/debian/.git-credentials && \
+  cmd cp /root/.config/otvl_vlts/install_env /home/debian/.config/.otvl/install_env && \
+  cmd cp /root/.config/otvl_vlts/install_groups /home/debian/.config/.otvl/install_groups && \
   cmd chmod -R go-rwX /home/debian/.config/.otvl/.secrets /home/debian/.git-credentials && \
   cmd chown -R debian:debian /home/debian && \
   cmd su - debian -c "$sd/as_deb_install_ansible.sh $CI_LOPS_REPO" && \
