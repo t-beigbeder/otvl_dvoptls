24e466bae204ffa64c56f2a48c48469fe2a5319e
thierry_otvl <tbeigbeder.nospam@gmail.com>
2025-06-03 16:01:49 +0200

    fx

--- install_scripts/common/as_deb_install_ansible.sh
+++ install_scripts/common/as_deb_install_ansible.sh
@@ -10,7 +10,7 @@ as_deb_install_ansible() {
   cd $sd/../../ansible && \
   cmd make venv-ins && \
   cmd git config --global credential.helper store && \
-  cmd cd locgit && git clone --single-branch $1 && \
+  cmd cd $HOME/locgit && git clone --single-branch $1 && \
   true
   return $?
 }
