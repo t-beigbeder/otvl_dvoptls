5f7984e7eaac73f4b368f750b8f112b3e34c0357
dvo <dvo@none.org>
2025-07-14 13:38:43 +0000

    cs_ops wip

--- docker/cs_ops/cs_ops.sh
+++ docker/cs_ops/cs_ops.sh
@@ -29,13 +29,64 @@ cmd() {
 
 cso_restore() {
     log restore $1
-    sleep 10
+    sleep 5
 }
 
-cso_backup() {
-    log backup $1
+mon_term() {
+    log mon_term
+    cmd kill $vpidh $vpidt $vpidd
+}
+
+mon_wait() {
+    log mon_wait start
+    wait
+    vst=$?
+    log mon_wait done $vst
+    return $vst
+}
+
+cso_backup_monitor() {
+    log backup_monitor
+    $0 backup home &
     vpidh=$!
-    sleep 10
+    sleep 1
+    $0 backup tools &
+    vpidt=$!
+    sleep 1
+    $0 backup data &
+    vpidd=$!
+    sleep 1
+    trap mon_term TERM INT
+    while [ true ] ; do
+        mon_wait && return 0
+    done
+}
+
+backup_term() {
+    log backup_term $vdir
+    exit 0
+}
+
+do_backup_dir() {
+    vdd=$1
+    vssho="ssh -o StrictHostKeyChecking=no -i /configmap/sprik"
+    #vcl=rsync -e "$vssho"
+    log do_backup_dir $vdd start
+    cmd sleep 10
+    log do_backup_dir $vdd done
+}
+
+cso_backup_dir() {
+    vdir=$1
+    vssho="ssh -o StrictHostKeyChecking=no -i /configmap/sprik"
+    #vcl=rsync -e "$vssho"
+    log backup_dir $1 start
+    while [ true ] ; do
+        trap - TERM
+        cmd sleep 7
+        trap backup_term TERM
+        cmd do_backup_dir $vdir
+    done
 }
 
 if [ $# -ne 2 ] ; then
@@ -47,7 +98,13 @@ if [ $1 = restore ] ; then
     exit 0
 fi
 if [ $1 = backup ] ; then
-    cso_backup $2 || fat $0 failed
-    log $@ stopping
-    exit 0    
+    if [ $2 = all ] ; then
+        cso_backup_monitor || fat $0 failed
+        log $@ stopping
+        exit 0    
+    else
+        cso_backup_dir $2 || fat $0 failed
+        log $@ stopping
+        exit 0    
+    fi
 fi
