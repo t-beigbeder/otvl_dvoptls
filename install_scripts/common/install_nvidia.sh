14f7ea87498ed86ec414d1701374a4e16c30ded9
thierry_otvl <tbeigbeder.nospam@gmail.com>
2025-08-23 20:27:41 +0200

    moved nvidia reboot

--- install_scripts/common/install_nvidia.sh
+++ install_scripts/common/install_nvidia.sh
@@ -14,10 +14,6 @@ if [ -z "`grep has_gpu /root/.config/otvl_vlts/install_otvl_meta`" ] ; then
     err $CI_HN_LIST has no GPU
     fat $0 failed
 fi
-if [ -f /root/nvidia-installed ] ; then
-    log $0 has restarted, nothing to do
-    exit 0
-fi
 export DEBIAN_FRONTEND=noninteractive
 cmd cd /tmp && \
 cmd wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb && \
@@ -29,9 +25,6 @@ cmd apt-get install -y software-properties-common && \
 cmd add-apt-repository -y contrib && \
 cmd apt-get install -y cuda-toolkit-13-0 && \
 cmd apt-get install -y nvidia-driver-cuda nvidia-kernel-dkms && \
-cmd touch /root/nvidia-installed && \
-log $0 will reboot now && \
-cmd reboot && \
 true || fat $0 failed
 log $0 stopping
 cmd sleep 60
