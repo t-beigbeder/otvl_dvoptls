d413fe8db722522fccc1f3b7ea3241270a6d3fee
thierry_otvl <tbeigbeder.nospam@gmail.com>
2025-08-31 15:01:47 +0200

    tofu prod

---
+++ tf/otvl/prod/hosting/main.tf
@@ -0,0 +1,40 @@
+provider "openstack" {
+}
+
+terraform {
+  required_version = ">= 1.9.0, < 2.0.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 3.0.0"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "~> 1.1.1"
+    }
+  }
+}
+
+module "network" {
+  source              = "../../modules/hosting/network"
+  ext_net_name        = var.ext_net_name
+  hosting_sg_name     = var.hosting_sg_name
+  hosting_ssh_exposed = var.hosting_ssh_exposed
+}
+
+module "compute" {
+  source              = "../../modules/hosting/compute"
+  ext_net_id          = module.network.ext_net_id
+  hosting_sg_id       = module.network.hosting_sg_id
+  hosting_ssh_exposed = var.hosting_ssh_exposed
+  hosting_cs_dvo      = var.hosting_cs_dvo
+  vlts_hostname       = var.vlts_hostname
+  vlts_port           = var.vlts_port
+  ssh_key_name        = var.ssh_key_name
+  ssh_pub             = var.ssh_pub
+  dot_repo            = var.dot_repo
+  dot_branch          = var.dot_branch
+  lops_repo           = var.lops_repo
+  install_env         = var.install_env
+  instances_attrs     = var.instances_attrs
+}
---
+++ tf/otvl/prod/hosting/variables.tf
@@ -0,0 +1,73 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+variable "ext_net_name" {
+  description = "The name of the external network"
+  type        = string
+}
+variable "hosting_sg_name" {
+  description = "The security group name for external access"
+  type        = string
+}
+variable "ssh_key_name" {
+  description = "The SSH key name to store the ssh_pub public key"
+  type        = string
+}
+variable "ssh_pub" {
+  description = "The SSH public key to authorize in created instances"
+  type        = string
+}
+variable "dot_repo" {
+  description = "Git repo devopstools"
+  type        = string
+}
+variable "dot_branch" {
+  description = "Git branch devopstools"
+  type        = string
+}
+variable "lops_repo" {
+  description = "Git repo devopstools local operations"
+  type        = string
+}
+variable "install_env" {
+  description = "Environment to install"
+  type        = string
+}
+variable "vlts_hostname" {
+  description = "The hostname or IP address of vlts"
+  type        = string
+}
+
+variable "instances_attrs" {
+  description = "Attributes for instances to create"
+  type = list(object({
+    name          = string
+    groups        = string
+    otvl_meta     = string
+    image_name    = string
+    flavor_name   = string
+    is_sync_server = optional(bool, false)
+    sync_disk_size = optional(number, 0)
+  }))
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+variable "vlts_port" {
+  description = "The HTTPS port of vlts"
+  type        = number
+  default     = 9443
+}
+variable "hosting_ssh_exposed" {
+  description = "do the computing hosts expose SSH"
+  type        = bool
+  default     = false
+}
+variable "hosting_cs_dvo" {
+  description = "do the computing hosts launch code-server for devops admin"
+  type        = bool
+  default     = false
+}
