1373273e16ea4cdabe061703bc3e17df84068436
dvo <dvo@none.org>
2025-10-11 16:40:07 +0000

    install container toolkit

--- install_scripts/common/install_nvidia.sh
+++ install_scripts/common/install_nvidia.sh
@@ -15,6 +15,7 @@ if [ -z "`grep has_gpu /root/.config/otvl_vlts/install_otvl_meta`" ] ; then
     fat $0 failed
 fi
 export DEBIAN_FRONTEND=noninteractive
+export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1
 cmd cd /tmp && \
 cmd wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb && \
 cmd dpkg -i cuda-keyring_1.1-1_all.deb && \
@@ -25,5 +26,20 @@ cmd apt-get install -y software-properties-common && \
 cmd add-apt-repository -y contrib && \
 cmd apt-get install -y cuda-toolkit-13-0 && \
 cmd apt-get install -y nvidia-driver-cuda nvidia-kernel-dkms && \
+cmd wget https://nvidia.github.io/libnvidia-container/gpgkey && \
+cmd rm /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg && \
+cmd gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg < gpgkey && \
+cmd rm gpgkey && \
+cmd wget https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list && \
+cmd sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
+  < nvidia-container-toolkit.list \
+  > /etc/apt/sources.list.d/nvidia-container-toolkit.list && \
+cmd rm nvidia-container-toolkit.list && \
+cmd apt-get update && \
+cmd apt-get install -y \
+      nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+      nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+      libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+      libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION} && \
 true || fat $0 failed
 log $0 stopping
