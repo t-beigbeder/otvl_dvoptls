eec96b180f8dded7942abb2d044dd711dfc7eb5d
dvo <dvo@none.org>
2025-10-12 20:11:03 +0000

    automatic install nvidia gpu operator with ansible

---
+++ install_scripts/common/install_gpu_operator.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+## pre
+rp=`realpath $0`
+sd=`dirname $rp`
+. $sd/../locenv
+## endpre
+
+log $0 starting
+cmd helm repo add nvidia https://helm.ngc.nvidia.com/nvidia && \
+cmd helm repo update && \
+cmd helm install --wait --generate-name \
+    -n gpu-operator --create-namespace \
+    nvidia/gpu-operator \
+    --version=v25.3.4 \
+    --set driver.enabled=false \
+    --set toolkit.enabled=false \
+    --set toolkit.env[0].name=CONTAINERD_CONFIG \
+    --set toolkit.env[0].value=/var/lib/rancher/k3s/agent/etc/containerd/config.toml \
+    --set toolkit.env[1].name=CONTAINERD_SOCKET \
+    --set toolkit.env[1].value=/run/k3s/containerd/containerd.sock && \
+true || fat $0 failed
+log $0 stopping
